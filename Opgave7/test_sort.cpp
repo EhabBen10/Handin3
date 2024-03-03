500792d14cf7f3a54a19dfef1a473795c1ca6592
Ehabben10 <202107666@post.au.dk>
2024-03-03 14:40:56 +0100

    opgave7

---
+++ Opgave7/test_sort.cpp
@@ -0,0 +1,60 @@
+#include "quick_sort.h"
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <vector>
+using namespace std;
+using namespace std::chrono;
+
+template <typename Comparable>
+void show(vector<Comparable> v)
+{
+    cout << "[";
+    for (Comparable i : v)
+        cout << v[i] << ", ";
+    cout << "]";
+}
+
+int main()
+{
+    int n_values[] = {10, 100, 1000, 10000};
+
+    for (int n : n_values)
+    {
+        vector<int> values(n);
+        cout << values.size() << endl;
+        // Generate Random values
+        auto f = []() -> int
+        { return rand() % 10000; };
+
+        // Fill up the vector
+        generate(values.begin(), values.end(), f);
+
+        // Get starting timepoint
+        auto start = high_resolution_clock::now();
+        insertionSort(values);
+        auto stop = high_resolution_clock::now();
+
+        auto duration = duration_cast<microseconds>(stop - start);
+
+        cout << "Time taken by function when insertSort: ";
+        cout << duration.count() << endl;
+
+        start = high_resolution_clock::now();
+        quickSort(values);
+        stop = high_resolution_clock::now();
+
+        duration = duration_cast<microseconds>(stop - start);
+        cout << "Time taken by function when quickinsert: ";
+        cout << duration.count() << endl;
+
+        start = high_resolution_clock::now();
+        show(values);
+        stop = high_resolution_clock::now();
+        duration = duration_cast<microseconds>(stop - start);
+        cout << "Time taken by function when stl sort: ";
+        cout << duration.count() << endl;
+    }
+
+    return 0;
+}
